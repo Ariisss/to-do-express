a60ea40ffb9688f56908257faeac027ece695e98
Prince Isaac Pantino <arispantino@gmail.com>
2024-11-08 00:13:42 +0800

    feat: implemented jwt token validation

--- src/utils/jwt.utils.ts
+++ src/utils/jwt.utils.ts
@@ -1,18 +1,10 @@
 import jwt from 'jsonwebtoken';
 import env from '../config/environment';
-import { User } from '../types/types';
 
-export function generateToken(user: Omit<User, 'password_hash'>): string {
-  return jwt.sign(
-    { 
-      id: user.id,
-      email: user.email 
-    },
-    env.JWT_SECRET,
-    { expiresIn: '24h' }
-  );
-}
+export const generateToken = (payload: object): string => {
+  return jwt.sign(payload, env.JWT_SECRET, { expiresIn: '1h' });
+};
 
-export function verifyToken(token: string): jwt.JwtPayload {
-  return jwt.verify(token, env.JWT_SECRET) as jwt.JwtPayload;
-}
+export const verifyToken = (token: string): any => {
+  return jwt.verify(token, env.JWT_SECRET);
+};
--- src/utils/password.utils.ts
+++ src/utils/password.utils.ts
@@ -1,14 +1,13 @@
 import bcrypt from 'bcrypt';
 
-const SALT_ROUNDS = 10;
+export const hashPassword = async (password: string): Promise<string> => {
+  const saltRounds = 10;
+  return await bcrypt.hash(password, saltRounds);
+};
 
-export async function hashPassword(password: string): Promise<string> {
-  return bcrypt.hash(password, SALT_ROUNDS);
-}
-
-export async function comparePasswords(
-  password: string, 
-  hashedPassword: string
-): Promise<boolean> {
-  return bcrypt.compare(password, hashedPassword);
-}
+export const comparePassword = async (
+  password: string,
+  hash: string
+): Promise<boolean> => {
+  return await bcrypt.compare(password, hash);
+};
