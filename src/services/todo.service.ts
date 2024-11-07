c481dcc41e3df989613d6153360ac9dab61de282
Prince Isaac Pantino <arispantino@gmail.com>
2024-11-08 00:43:30 +0800

    feat: added underscore case on parameters

--- src/services/todo.service.ts
+++ src/services/todo.service.ts
@@ -1,12 +1,12 @@
 import { Todo } from '../models/todo.model';
 
 export const createTodo = async (userId: number, data: any) => {
-  const todo = await Todo.create({ ...data, userId });
+  const todo = await Todo.create({ ...data, user_id: userId });
   return todo;
 };
 
 export const getTodos = async (userId: number) => {
-  const todos = await Todo.findAll({ where: { userId } });
+  const todos = await Todo.findAll({ where: { user_id: userId } });
   return todos;
 };
 
@@ -15,14 +15,14 @@ export const updateTodo = async (
   todoId: number,
   data: any
 ) => {
-  const todo = await Todo.findOne({ where: { id: todoId, userId } });
+  const todo = await Todo.findOne({ where: { id: todoId, user_id: userId } });
   if (!todo) throw new Error('Todo not found');
   await todo.update(data);
   return todo;
 };
 
 export const deleteTodo = async (userId: number, todoId: number) => {
-  const todo = await Todo.findOne({ where: { id: todoId, userId } });
+  const todo = await Todo.findOne({ where: { id: todoId, user_id: userId } });
   if (!todo) throw new Error('Todo not found');
   await todo.destroy();
   return;
