a6fa1f85d87b08ab300c05c0f85a69d640058dc1
Prince Isaac Pantino <arispantino@gmail.com>
2024-11-07 12:20:21 +0800

    feat: moved database initialization logic to database.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 import env from "./config/environment";
-const app = express();
+import { initializeDatabase } from './config/database';
 
+const app = express();
 const port = env.PORT;
 
-app.get("/", (req: Request, res: Response) => {
+// Initialize database before starting server
+initializeDatabase().then(() => {
+  app.get("/", (req: Request, res: Response) => {
     res.send("Hello World");
-});
+  });
 
-app.listen(port, () => {
+  app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+  });
 });
 
